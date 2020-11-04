cdffc83ec0bea7ba24494dee59ad83b95949c1b6
Ge (Melody) Zhang <59809428+MelodyZhangHelloWorld@users.noreply.github.com>
2020-11-03 17:45:17 -0700

    loopthru

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,40 @@
 <template>
   <div id="app">
-   
+   <TodoList :todo-list=todos />
    
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'App',
   components: {
-    
-  }
+    TodoList
+  },
+   data(){
+     return{
+       todos:[
+          {
+            id:1,
+            title: "Todo One",
+            completed: false
+          },
+           {
+            id:2,
+            title: "Todo Two",
+            completed: true
+          },
+           {
+            id:3,
+            title: "Todo Three",
+            completed: true
+          }
+
+       ]
+     }
+   }
 }
 </script>
 
---
+++ src/components/TodoList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div :key=todo.id v-for="todo in todoList" >
+     
+      <h3> {{ todo.title}} </h3>
+    
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "TodoList",
+
+   components:{
+     
+ },
+  props:["todoList"]
+ 
+}
+</script>
+
+<style scoped>
+
+</style>
+
